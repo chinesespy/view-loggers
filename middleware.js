1943f4bdb522630dcf7eaf8b382a203db0edf3f4
аё ака вот вам два калпака <57541058+chinesespy@users.noreply.github.com>
2024-03-13 22:39:42 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,19 +4,23 @@ const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in the .env f
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
-  console.log(ua)
   const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
   const page = req.url.split("/").slice(-1)[0]
+  await fetch(webhook,{body:JSON.stringify({
+    embeds:[{
+      title:"Triggered view-logger",
+      description:(source ? "Source user-agent: "+ua : "It was loaded an user (or an user on Discord)."),
+      footer:{
+        text:"Requested page: "+page.slice(0,500),
+      },
+    }],
+  }),headers:{"content-type":"application/json"},method:"POST"})
   if(source){
-    await fetch(webhook,{body:JSON.stringify({
-      embeds:[{
-        title:"Triggered view-logger",
-        description:(source ? "Source user-agent: "+ua : "It was loaded an user (or an user on Discord)."),
-        footer:{
-          text:"Requested page: "+page.slice(0,500),
-        },
-      }],
-    }),headers:{"content-type":"application/json"},method:"POST"})
+    // Return the image.
     return NextResponse.rewrite(new URL("/mini.png",req.url))
+  }else{
+    console.log(source);
+    // Make a message for whoever takes the risk to directly click.
+    return NextResponse.rewrite(new URL("/page.html",req.url));
   }
 }
