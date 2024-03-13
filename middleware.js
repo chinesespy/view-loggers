6ea46cc021c12a38f3e9b0f3c8f4846f653c807c
аё ака вот вам два калпака <57541058+chinesespy@users.noreply.github.com>
2024-03-13 22:31:33 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -7,18 +7,16 @@ export async function middleware(req){
   console.log(ua)
   const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
   const page = req.url.split("/").slice(-1)[0]
-  await fetch(webhook,{body:JSON.stringify({
-    embeds:[{
-      title:"Triggered view-logger",
-      description:(source ? "Source user-agent: "+ua : "It was loaded an user (or an user on Discord)."),
-      footer:{
-        text:"Requested page: "+page.slice(0,500),
-      },
-    }],
-  }),headers:{"content-type":"application/json"},method:"POST"})
   if(source){
-    // Return the image.
+    await fetch(webhook,{body:JSON.stringify({
+      embeds:[{
+        title:"Triggered view-logger",
+        description:(source ? "Source user-agent: "+ua : "It was loaded an user (or an user on Discord)."),
+        footer:{
+          text:"Requested page: "+page.slice(0,500),
+        },
+      }],
+    }),headers:{"content-type":"application/json"},method:"POST"})
     return NextResponse.rewrite(new URL("/mini.png",req.url))
   }
-  NextResponse.next();
 }
